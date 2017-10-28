bb24f0dd63e9912dbaa11a22ecd8723522f1aa83
claitoncs <custodio.claiton@gmail.com>
2017-10-27 22:07:22 -0200

    atualizando teste

--- oData.js
+++ oData.js
@@ -3,12 +3,11 @@ var oData = {
     files: []
 }
 
-
 oData['files'].push({ path: ['dir1','dir2'], name:'arq.txt', file_length: 6, bin: '012345', owner: 'claiton' });
 oData['files'].push({ path: [], name:'arq1.txt', bin: '1012345', file_length: 7, owner: 'claiton' });
 oData['files'].push({ path: ['dir1'], name:'arq2.txt', bin: '012345', file_length: 7, owner: 'claiton' });
 oData['files'].push({ path: ['dir1','dir3'], name:'arq3.txt', bin: '012345', file_length: 7, owner: 'claiton' });
 
-console.log(oData);
+//console.log(oData);
 
 exports.oData=oData;
