50f5ae167924612317bb6c99ab365a43e896df08
claitoncs <custodio.claiton@gmail.com>
2017-11-05 17:32:44 -0200

    inicio...

--- main.js
+++ main.js
@@ -2,7 +2,86 @@
     
 var oData = require('./oData');
 
+
+
+////////////////////////////////////////////////////////////////////////////////
+// 1. Obtem o ip do computador local na rede  e verifica se há conexão de rede...
+////////////////////////////////////////////////////////////////////////////////
+
+var os = require('os');
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+oData['myIP'] = addresses[0];
+if (typeof oData['myIP'] === 'undefined') {
+    console.log ('No network found!');
+    process.exit(0);
+}
+console.log('Network ok. MyIP:' + oData['myIP']);
 console.log(oData);
+
+
+////////////////////////////////////////////////////////////////////////////////
+// 2.  Ao iniciar a aplicação, inicializa o servidor UDP, para escutar sobre 
+//     outros nós entrando 
+////////////////////////////////////////////////////////////////////////////////
+
+var dgram = require('dgram');
+var serverUDP = dgram.createSocket('udp4');
+
+serverUDP.on('listening', function () {
+    var address = serverUDP.address();
+    serverUDP.setBroadcast(true);
+    console.log('UDP Server listening for broadcasts on ' + address.address + ":" + address.port);
+});
+
+serverUDP.on('message', function (message, remote) {
+   //recebeu mensagem broadcast udp
+   //conecta com o outro par via tcp
+   console.log('Vai estabelecer a conexão TCP com o outro computador!');
+   
+/*   
+   if ( typeof dadosCompartilhados[2]['usuarios'].find(o => o.ip === remote.address) === 'undefined') {
+       //fará a inclusão ip no objeto de usuários
+       dadosCompartilhados[2]['usuarios'].push({ cod: v_cod++, 
+                                                  ip: remote.address, 
+                                                  usuario: '' +message+'',
+                                                  online: 'N' });
+   } else {
+       console.log('Recebeu pedido de conexao udp com ip repetido...');
+   }
+   
+    console.log('Usuario do endereço ' + remote.address + ':' + remote.port +' -  enviou mensagem: ' + message);
+*/
+
+});
+
+serverUDP.on('error', function(err) {
+    if (err.code === 'EADDRINUSE') {
+    console.log('Erro na inicialização do servidor UDP.');
+    }
+}
+);
+
+
+
+var client = dgram.createSocket({type:"udp4",reuseAddr:true});
+client.bind();
+client.on("listening", function () {
+    client.setBroadcast(true);
+    client.send('teste', 0, 'teste'.length, 8080, '255.255.255.255', function(err, bytes) {
+        client.close();
+    });
+});
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // 1. Obtem o ip do computador local na rede  e verifica se há conexão de rede...
