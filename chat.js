816d0909d0f2fbd450550d0ee5e371629de9f727
claitoncs <custodio.claiton@gmail.com>
2017-12-01 12:48:04 -0200

    outro teste

--- chat.js
+++ chat.js
@@ -60,31 +60,45 @@ for (var k in interfaces) {
 }
 
 var meuIP = addresses[0];
-if (typeof oData['myIP'] === 'undefined') {
+if (typeof meuIP === 'undefined') {
     console.log ('No network found!');
     process.exit(0);
 }
 console.log('Network ok. MyIP:' + meuIP);
 
+
 //Assumindo que já identificamos os pontos em um vetor... isso será feito no broadcast
 var pontos = ['172.20.10.4', '172.20.10.10', '172.20.10.11'];
 
-pontos.forEach(function (ponto) {
-    if (ponto===meuIP) return;
+while (true) {
 
-    var oclient = new net.Socket();
-    oclient.connect(5000, ponto, function() {
-        console.log('Connected');
-        oclient.write('Hello, server! Love, Client.' + meuIP);
-    });
-    
-    oclient.on('data', function(data) {
-        console.log('Como cliente, Recebi: ' + data);
-        
-    });
-    
-    oclient.on('close', function() {
-        console.log('Connection closed');
-    });
+        console.log('teste');
+        pontos.forEach(function (ponto) {
+            if (ponto===meuIP) return;
 
-});
+            var oclient = new net.Socket();
+            oclient.connect(5000, ponto, function() {
+                console.log('Connected');
+                oclient.write('Hello, server! Love, Client.' + meuIP);
+            });
+            
+            oclient.on('data', function(data) {
+                console.log('Como cliente, Recebi: ' + data);
+                
+            });
+            
+            oclient.on('close', function() {
+                console.log('Connection closed');
+            });
+/*
+            oclient.on('error', function(err) {
+                if (err.code === 'ECONNREFUSED') {
+                console.log('Servidor remoto ainda não está no ar. Esperará 10 secs e tentará de novo...');
+                //var waitTill = new Date(new Date().getTime() + seconds * 1000);
+                //while(waitTill > new Date()){}
+                }
+            }
+            );
+*/
+        });
+}
