f844872a8dad473dfba72b941f31a341b2b7a2d2
claitoncs <custodio.claiton@gmail.com>
2017-12-01 11:17:08 -0200

    Adicionou interceptação de sinal à saída do processo para fechar o socket.

--- clientetcp.js
+++ clientetcp.js
@@ -8,9 +8,31 @@ client.connect(5000, '172.20.10.4', function() {
 
 client.on('data', function(data) {
 	console.log('Received: ' + data);
-	//client.destroy(); // kill client after server's response
+	
 });
 
 client.on('close', function() {
 	console.log('Connection closed');
 });
+
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(options, err) {
+    client.destroy(); // fecha o socket ao sair.
+    if (options.cleanup) console.log('clean');
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
